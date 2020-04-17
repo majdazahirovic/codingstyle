1c8d915b12d2c3c20f308d7db96f13a0836262e2
Majda Zahirovic <majda.zahirovic@hotmail.com>
2020-04-17 18:47:17 +0200

    +containsAllIgnoreCase +containsAllIgnoreCase(Junit) +SetTest(Junit)

--- src/test/java/edu/hm/hafner/util/SetTest.java
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class SetTest {
 
-    HashSet<Integer> creatHashSetFilled(final Integer... filled) {
+    HashSet<Integer> createHashSetFilled(final Integer... filled) {
         return new HashSet<>(asList(filled));
     }
 
@@ -18,24 +18,83 @@ public class SetTest {
         return new HashSet<>();
     }
 
-
     @Test
-    void testAdd(){
-        HashSet<Integer> testObjectAdd = creatHashSetFilled();
+    void testAdd() {
+        HashSet<Integer> testObjectAdd = createHashSetFilled();
 
         assertThat(testObjectAdd.add(1)).isTrue();
         assertThat(testObjectAdd.add(1)).isFalse();
+        assertThat(testObjectAdd.add(2)).isTrue();
+        assertThat(testObjectAdd.add(-1)).isTrue();
     }
-    void testClear(){}
-    void testClone(){}
-    void testContain(){}
-    void testIsEmpty(){}
-    void testSize(){}
-    void testIsRemove(){}
 
+    @Test
+    void testClear() {
+        HashSet<Integer> testObjectClear = createHashSetEmpty();
 
+        testObjectClear.add(1);
+        testObjectClear.add(2);
+        testObjectClear.add(3);
 
+        assertThat(testObjectClear).isNotEmpty();
 
+        testObjectClear.clear();
+
+        assertThat(testObjectClear).isEmpty();
+
+    }
+
+    @Test
+    void testClone() {
+
+        HashSet<Integer> testObjectClone = createHashSetFilled(1, 2, 3);
+        HashSet<Integer> testObjectClone2 = (HashSet<Integer>) testObjectClone.clone();
+
+        assertThat(testObjectClone).isEqualTo(testObjectClone2);
+
+    }
+
+    @Test
+    void testContain() {
+
+        HashSet<Integer> testObjectContain = createHashSetFilled(1, 2, 3);
+
+        assertThat(testObjectContain.contains(1)).isTrue();
+        assertThat(testObjectContain.contains(5)).isFalse();
+        assertThat(testObjectContain.contains(0)).isFalse();
+        assertThat(testObjectContain.contains(3)).isTrue();
+        assertThat(testObjectContain.contains(null)).isFalse();
+
+    }
+
+    @Test
+    void testIsEmpty() {
+
+        HashSet<Integer> testObjectEmptyFilled = createHashSetFilled(1, 2, 3, 4);
+        HashSet<Integer> testObjectEmpty = createHashSetEmpty();
+
+        assertThat((testObjectEmptyFilled).isEmpty()).isFalse();
+        assertThat((testObjectEmpty).isEmpty()).isTrue();
+
+    }
+
+    @Test
+    void testSize() {
+        HashSet<Integer> testObjectSize = createHashSetFilled(1, 2, 3, 4);
+        assertThat(testObjectSize.size()).isEqualTo(4);
+    }
+
+    @Test
+    void testIsRemove() {
+
+        HashSet<Integer> testObjectRemove = createHashSetFilled(1, 2, 3, 4);
+
+        assertThat(testObjectRemove.remove(1)).isTrue();
+        assertThat(testObjectRemove.remove(6)).isFalse();
+        assertThat(testObjectRemove.remove(null)).isFalse();
+        assertThat(testObjectRemove.remove(3)).isTrue();
+
+    }
 
 }
 
